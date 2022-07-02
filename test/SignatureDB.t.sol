1695468215d483332b421c346bf3ad7c2d072d88
Will Papper <contact@papper.me>
2022-07-02 17:21:39 +0200

    Updated Solidity version numbers + added test stub

---
+++ test/SignatureDB.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import "../lib/forge-std/src/Test.sol";
+
+contract SignatureDBTest is Test {
+    function setUp() public {}
+
+    function testExample() public {
+        assertTrue(true);
+    }
+}

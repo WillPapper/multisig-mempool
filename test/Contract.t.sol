e9e20aec32242d2760c3d44ab91a07c0eacbcc19
Will Papper <contact@papper.me>
2022-07-03 19:00:54 +0200

    Added additional mainnet fork checks

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -10,9 +10,30 @@ contract ContractTest is Test {
         assertTrue(true);
     }
 
-    function testMainnetFork() public {
+    function testMainnetForkBalance() public {
         assertTrue(
             address(0x000000000000000000000000000000000000dEaD).balance > 0
         );
+    }
+
+    function testMainnetForkContract() public {
+        // Assert that 0xdEaD is not a contract
+        assertTrue(
+            !isContract(address(0x000000000000000000000000000000000000dEaD))
+        );
+
+        // Assert that the ConstitutionDAO Gnosis Safe is a contract
+        assertTrue(
+            isContract(address(0xb1C95AC257029D11F3f64ac67b2307A426699322))
+        );
+    }
+
+    // From https://github.com/OpenZeppelin/openzeppelin-contracts/blob/5fbf494511fd522b931f7f92e2df87d671ea8b0b/contracts/utils/Address.sol#L36
+    function isContract(address account) internal view returns (bool) {
+        // This method relies on extcodesize/address.code.length, which returns 0
+        // for contracts in construction, since the code is only stored at the end
+        // of the constructor execution.
+
+        return account.code.length > 0;
     }
 }

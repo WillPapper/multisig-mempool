ee5ae4ee55c4b47f1fc2f51226d93211520b65e2
Will Papper <contact@papper.me>
2022-07-04 13:32:37 +0200

    Added SignatureDB and skeleton of tests (#1)
    * Added stub of SignatureDB
    
    * forge install: safe-contracts
    
    main
    
    * Added initializeTransaction() and addSignature()
    
    * Added note that the set of signers could change
    
    * Updated Solidity version numbers + added test stub
    
    * WIP - Adding tests for Gnosis Safe Proxy Factory
    
    * Fixed Gnosis Safe initialization
    
    * Removed old comments
    
    * Added todo for events
    
    * Added additional mainnet fork checks

---
+++ src/SignatureDB.sol
@@ -0,0 +1,105 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import {GnosisSafe} from "../lib/safe-contracts/contracts/GnosisSafe.sol";
+
+contract SignatureDB {
+    // Mapping of Gnosis Safe address => dataHash => Transaction
+    mapping(address => mapping(bytes32 => Transaction)) transactionsForGnosisSafe;
+
+    // We don't need the dataHash because that is used as the key in the mapping.
+    struct Transaction {
+        bytes data;
+        bytes signatures;
+    }
+
+    // TODO: Add events
+
+    function initializeTransaction(
+        address gnosisSafe,
+        bytes32 dataHash,
+        bytes calldata data,
+        bytes calldata signatures
+    ) public {
+        checkSignaturesGnosis(gnosisSafe, dataHash, data, signatures);
+
+        // Once we have checked the signatures, we can store the initial transaction
+        Transaction memory transaction = Transaction(data, signatures);
+        transactionsForGnosisSafe[gnosisSafe][dataHash] = transaction;
+    }
+
+    /// @notice You can add one or multiple signatures via concatenated bytes
+    /// @dev The only requirement for `signatures` is that it is greater than
+    /// the previous bytes of signatures. This means that the set of signers
+    /// could change. So this does not strictly always add signatures. If the
+    /// new set of signatures is greater than the previous set of signatures but
+    /// has different signers, the signers from the previous set that are not
+    /// represented in the new set will be removed. What this does guarantee,
+    /// however, is that calling `addSignatures()` will always ensure that a
+    /// transaction is closer to execution than before.
+    /// TODO: Handle signature insertion on-chain, which ensure that signatures
+    /// are always increasing.
+    function addSignatures(
+        address gnosisSafe,
+        bytes32 dataHash,
+        bytes calldata signatures
+    ) public {
+        Transaction memory transaction = transactionsForGnosisSafe[gnosisSafe][
+            dataHash
+        ];
+
+        // Check whether the transaction has been initialized.
+        // We can't initialize the transaction for the user here because we
+        // don't have the data available. We avoid passing in the data to save
+        // on calldata costs, because the data is only necessary when a
+        // signature is initialized and then afterward is kept in storage
+        require(
+            transaction.signatures.length != 0,
+            "Transaction must be initialized"
+        );
+
+        // Place the new signatures at the start of the concatenated bytes
+        // This ensures that any failure in the new signatures will
+        // short-circuit. As a result, gas will not be wasted checking the old
+        // signatures.
+        // TODO: You'll need to deconstruct, sort, and insert these signatures
+        // on-chain to ensure that previous signatures are never removed
+        bytes memory signaturesConcat = bytes.concat(
+            signatures,
+            transaction.signatures
+        );
+
+        // Check that the new data is valid
+        // TODO: Confirm whether you can check only `signatures` for the new
+        // signatures, without needing to check all of `signaturesConcat`. You'd
+        // need to manually check whether the signatures meet the transaction
+        // threshold, which as a benefit also allows you to cut off signing when
+        // the transaction threshold is reached.
+        checkSignaturesGnosis(
+            gnosisSafe,
+            dataHash,
+            transaction.data,
+            signaturesConcat
+        );
+
+        transactionsForGnosisSafe[gnosisSafe][dataHash]
+            .signatures = signaturesConcat;
+    }
+
+    function checkSignaturesGnosis(
+        address gnosisSafe,
+        bytes32 dataHash,
+        bytes memory data,
+        bytes memory signatures
+    ) public view {
+        // Check that the initial transaction is valid
+        // This will fail within GnosisSafe.checkNSignatures() if any signature is invalid
+        GnosisSafe(payable(gnosisSafe)).checkSignatures(
+            dataHash,
+            data,
+            signatures
+        );
+    }
+
+    function setSignatureReward() public payable {}
+}
